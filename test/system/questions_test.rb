52b4910837a25ef38808434a624bfe3045a8cd6b
Sue Rocha <swissbusybee@gmail.com>
2020-04-14 16:07:23 +0200

    Tests Complete

---
+++ test/system/questions_test.rb
@@ -0,0 +1,29 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "asking a question yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Can I?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+end
